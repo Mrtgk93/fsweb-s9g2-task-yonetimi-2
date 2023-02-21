3ece24e1d2cc4ac885c5cba9598902ca81a300de
Mrtgk93 <mrtgk93@hotmail.com>
2023-02-22 02:49:34 +0300

    task.js tailwind kullanılarak yeniden yazıldı

--- src/Task.js
+++ src/Task.js
@@ -18,21 +18,31 @@ const Task = ({ taskObj, onComplete }) => {
   let renk = Math.abs(b) <= 3 ? "bg-[#ffd9d4]" : "bg-green-600";
 
   return (
-    <div className="task">
-      <h3>{taskObj.title}</h3>
-      <div className="deadline">
+    <div className=" p-6 bg-white rounded-md leading-5 mt-4 shadow-md">
+      <h3 className=" text-lg text-amber-600 ">{taskObj.title}</h3>
+      <div className=" pt-1 text-xs">
         son teslim: <span className={renk}>{`${distance1} ${fark}`}</span>
       </div>
-      <p>{taskObj.description}</p>
+      <p className=" text-sm text-zinc-800 py-2 pt-0 pb-3">
+        {taskObj.description}
+      </p>
       <div>
         {taskObj.people.map((p) => (
-          <span className="pill" key={p}>
+          <span
+            className=" inline-block px-3 py-1 text-sm border-solid border-2 border-s rounded-3xl mr-1 mb-2 "
+            key={p}
+          >
             {p}
           </span>
         ))}
       </div>
       {onComplete && (
-        <button onClick={() => onComplete(taskObj.id)}>Tamamlandı</button>
+        <button
+          className=" block py-2 px-3 ml-auto bg-orange-300 shadow-lg rounded cursor-pointer"
+          onClick={() => onComplete(taskObj.id)}
+        >
+          Tamamlandı
+        </button>
       )}
     </div>
   );
